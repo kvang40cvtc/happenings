a8b48317b9d12dda01d20ed3a957cc5bb7f009b2
kvang40 <kvang40@student.cvtc.edu>
2026-04-17 17:49:49 -0500

    Initial servlet setup

---
+++ src/main/java/com/happenings/test/TestServlet.java
@@ -0,0 +1,24 @@
+package com.happenings.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/plain");
+
+        PrintWriter out = response.getWriter();
+        out.println("Happenings backend is working!");
+    }
+}
